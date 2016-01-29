031a08d220af619f01a6e43cd4633dbf1b471fdd
chavy25 <chavy.abarca@gmail.com>
2016-01-29 07:06:09 -0600

    Practica

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Progra3Ej2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtBool = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTercero = new System.Windows.Forms.TextBox();
+            this.txtCuarto = new System.Windows.Forms.TextBox();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(43, 59);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(100, 20);
+            this.txtEntrada.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSalir);
+            this.panel1.Controls.Add(this.btnBorrar);
+            this.panel1.Controls.Add(this.txtCuarto);
+            this.panel1.Controls.Add(this.txtTercero);
+            this.panel1.Controls.Add(this.txtBool);
+            this.panel1.Controls.Add(this.btnIngresar);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtEntrada);
+            this.panel1.Location = new System.Drawing.Point(48, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(338, 288);
+            this.panel1.TabIndex = 4;
+            // 
+            // txtBool
+            // 
+            this.txtBool.Location = new System.Drawing.Point(43, 100);
+            this.txtBool.Name = "txtBool";
+            this.txtBool.Size = new System.Drawing.Size(100, 20);
+            this.txtBool.TabIndex = 5;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(46, 226);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // txtTercero
+            // 
+            this.txtTercero.Location = new System.Drawing.Point(43, 140);
+            this.txtTercero.Name = "txtTercero";
+            this.txtTercero.Size = new System.Drawing.Size(183, 20);
+            this.txtTercero.TabIndex = 6;
+            // 
+            // txtCuarto
+            // 
+            this.txtCuarto.Location = new System.Drawing.Point(46, 175);
+            this.txtCuarto.Name = "txtCuarto";
+            this.txtCuarto.Size = new System.Drawing.Size(183, 20);
+            this.txtCuarto.TabIndex = 7;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(127, 226);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 8;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(208, 226);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 330);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.TextBox txtBool;
+        private System.Windows.Forms.TextBox txtTercero;
+        private System.Windows.Forms.TextBox txtCuarto;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Progra3Ej2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Programacion III";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+
+            txtEntrada.Text = "Hola Malaka";
+            
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            bool booleano = true;
+            txtBool.Text = booleano.ToString();
+            string myString = "HOOOOOOOOOOOOOOLAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+            txtTercero.Text = myString;
+
+            int[] arregloBi = { 5, 10, 12 };
+            foreach (int num in arregloBi)
+            {
+                
+                txtCuarto.Text+= " "+ num.ToString();
+            }
+           
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtBool.Text = "";
+            txtCuarto.Text = "";
+            txtEntrada.Text = "";
+            txtTercero.Text = "";
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
